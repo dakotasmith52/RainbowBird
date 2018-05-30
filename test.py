6124392146f7ad34d4722a330cf7d0a2d742b7ee
Dakota Smith <smith.dakota52@gmail.com>
2018-05-30 10:08:14 -0700

    Create recordme.py file, which is a simple audio recording program. Not yet complete, but in a working state.

---
+++ test.py
@@ -0,0 +1,19 @@
+import boto3
+import os
+import time
+
+
+my_location = "s3://jedijamez/projects/"
+
+
+# Record Audio when I press this button
+
+
+
+# Stop Recording when i press this button
+
+
+
+# Send audio file to my_location
+
+
